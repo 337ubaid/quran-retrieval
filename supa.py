f0afbdc7a2337c3b450f2c5a136c7272c2a758c6
337ubaid <337ubaid@gmail.com>
2025-02-03 22:17:53 +0700

    add supa.py

---
+++ supa.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from st_supabase_connection import SupabaseConnection, execute_query 
+
+# # Initialize connection.
+conn = st.connection("supabase",type=SupabaseConnection)
+
+# Perform query.
+rows = execute_query(conn.table("ayat_quran").select("*"), ttl=None)
+# conn.table("ayat_quran").select("*"), ttl=None
+# print(rows)
+# Print results.
+for row in rows.data:
+    st.write(f"{row['id']} : {row['indoText']}:")
